a4d66909e2e1b450f0d4f5cbce5a5956ab57b00a
Omkar Upadhyay <omkarmh009@gmail.com>
2024-02-25 11:51:39 +0800

    Initial Commit for Employee API

---
+++ src/test/java/com/ou_solutions/employeeapi/EmployeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ou_solutions.employeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
